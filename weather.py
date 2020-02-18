099ab4e35947a9b29a93fec6d553c3cb9a55e642
chijunyou <55154341+chijunyou@users.noreply.github.com>
2020-02-18 10:50:09 -0500

    save the api file, data file and test file

---
+++ weather.py
@@ -0,0 +1,60 @@
+import sys
+import csv
+import requests
+
+
+def AirportWeather(Airport, APIKey):
+	rtn = list()
+	findname = False
+	try:
+		with open('./data.csv', 'r') as f:
+			reader = csv.reader(f)
+			for row in reader:
+				if  Airport == row[3]:
+					lat = str(int(float(row[4])))
+					lon = str(int(float(row[5])))
+					findname = True
+					break
+	except:
+		rtn.append(0)
+		rtn.append("fail to find the name")
+		return rtn
+
+	if not findname:
+		rtn.append(0)
+		rtn.append("airportName is wrong.")
+		return rtn
+
+
+	try:
+		link = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey
+		jsonfile = requests.get(link).json()
+	except:
+		rtn.append(0)
+		rtn.append("fail to load weather information")
+		return rtn
+
+	
+	for i in range (0,9):
+		weatherinformation = list()
+		time = jsonfile['list'][i]["dt_txt"]
+		temperature = int(float(jsonfile['list'][i]['main']['temp']) - 273)
+		weather = jsonfile['list'][i]['weather'][0]['main']
+		weatherinformation.append(time)
+		weatherinformation.append(temperature)
+		weatherinformation.append(weather)
+		rtn.append(weatherinformation)
+
+
+
+	return rtn
+
+def main():
+
+	airportName = "Aero B Ranch Airporta"
+	APIKey = 
+	rtn = AirportWeather(airportName, APIKey)
+	print(rtn)
+
+if __name__=="__main__":
+  main()
