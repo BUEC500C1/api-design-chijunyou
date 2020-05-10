1386265d36a6c5c4f7bedfdc3f1bd66cdbb4d087
chijunyou <55154341+chijunyou@users.noreply.github.com>
2020-05-10 15:26:49 -0400

    Update hw2_test.py

--- hw2_test.py
+++ hw2_test.py
@@ -8,7 +8,7 @@ key = "f8f8eae96c8a82ebcd0b7e784460fe8f"
 
 def testcase():
 	assert AirportWeather("vasdfva", key)[0] == 0
-	assert AirportWeather("vafdvav", key)[1] == "fail to find the name"
+	assert AirportWeather("vafdvav", key)[1] == "airportName is wrong."
 	assert AirportWeather("Watts Field", 'wrongkey')[0] == 0
 	assert AirportWeather("Watts Field", 'wrongkey')[1] == "fail to load weather information"
 
