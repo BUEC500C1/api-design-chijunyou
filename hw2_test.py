b4c2673a597b40d67c42cc4544ec5ce36a47883c
chijunyou <55154341+chijunyou@users.noreply.github.com>
2020-05-10 15:24:41 -0400

    Add files via upload

---
+++ hw2_test.py
@@ -0,0 +1,14 @@
+from weather import AirportWeather
+import pytest
+
+
+
+
+key = "f8f8eae96c8a82ebcd0b7e784460fe8f"
+
+def testcase():
+	assert AirportWeather("vasdfva", key)[0] == 0
+	assert AirportWeather("vafdvav", key)[1] == "fail to find the name"
+	assert AirportWeather("Watts Field", 'wrongkey')[0] == 0
+	assert AirportWeather("Watts Field", 'wrongkey')[1] == "fail to load weather information"
+
